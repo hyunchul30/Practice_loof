ca3fd464066eaec13a28da14e6202065e007d8b0
Student <example@example.com>
2019-08-21 16:48:41 -0700

    test

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+
+
+$(document).ready(function(){
+  $("form #test").submit(function(event){
+  event.preventDefault();
+    });
+
+
+    $("#B").click(function() {
+    $("#bike").show();
+    });
+
+    $("#Password").click(function() {
+    ("p").hide();
+
+    });
---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+
+
+$(document).ready(function(){
+  $("form #test").submit(function(event){
+  event.preventDefault();
+    });
+
+
+    $("#B").click(function() {
+    $("#bike").hide();
+    });
+
+    $("#Password").click(function() {
+    ("p").show();
+
+    });
